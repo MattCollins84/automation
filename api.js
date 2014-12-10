3fd5d4f21f7cdd09dc5127bff84597f2665da01e
Matt Collins <matt@strikesandgutters.com>
2014-12-10 19:22:56 +0000

    script

--- api.js
+++ api.js
@@ -1,36 +1,32 @@
 var gpio = require('rpi-gpio');
 var async = require('async');
+var argv = require('optimist').argv
 
 var pins = [11, 15, 16, 13];
 var actions = [];
-var power = true;
+var power = (argv.power?true:false);
 
-gpio.setup(11, gpio.DIR_IN, function(err) {
-  gpio.read(11, function(err, data) {
-    return console.log(err, data);
-  });
+
+for (var p in pins) {
+
+  (function(pin, power) {
+
+    actions.push(function(callback) {
+
+      gpio.setup(pin, gpio.DIR_OUT, function(err) {
+        gpio.write(pin, power, function() {
+          return callback(err, null);
+        });
+      });
+
+    });
+
+  })(pins[p], power);
+
+}
+
+async.parallel(actions, function(err, results) {
+
+  console.log("done!");
+
 });
-
-// for (var p in pins) {
-
-//   (function(pin, power) {
-
-//     actions.push(function(callback) {
-
-//       gpio.setup(pin, gpio.DIR_OUT, function(err) {
-//         gpio.write(pin, power, function() {
-//           return callback(err, null);
-//         });
-//       });
-
-//     });
-
-//   })(pins[p], power);
-
-// }
-
-// async.parallel(actions, function(err, results) {
-
-//   console.log("done!");
-
-// });
