cdf6f873894fb836e8aae8e6a764ae269d482057
Matt Collins <matt@strikesandgutters.com>
2014-12-10 19:28:24 +0000

    read script

---
+++ read.js
@@ -0,0 +1,16 @@
+var gpio = require('rpi-gpio');
+var async = require('async');
+var argv = require('optimist').argv
+
+var pin = argv.pin;
+
+if (!pin) {
+  console.log("--pin <pin number> is required");
+  process.exit(0);
+}
+
+gpio.setup(pin, gpio.DIR_IN, function(err) {
+  gpio.read(pin, function(err, data) {
+    console.log(err, data);
+  });
+});
