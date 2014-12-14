5f7684afe2bfd063eee8fcd3ba2e9d38f9fd7921
Matt Collins <matt@strikesandgutters.com>
2014-12-14 16:49:21 +0000

    script

--- wpi.js
+++ wpi.js
@@ -1,19 +1,21 @@
 var wpi = require('wiring-pi');
 
-wpi.wiringPiSetupSys();
+wpi.wiringPiSetup();
+
+var pin = 0;
 
 // read the value
-wpi.pinMode(16, wpi.modes.INPUT);
-var val = wpi.digitalRead(16);
+wpi.pinMode(pin, wpi.modes.INPUT);
+var val = wpi.digitalRead(pin);
 console.log(val);
 
 var new_val = (1 - val);
 
 // write the value
-wpi.pinMode(16, wpi.modes.OUTPUT);
+wpi.pinMode(pin, wpi.modes.OUTPUT);
 console.log('writing '+new_val)
-wpi.digitalWrite(16, new_val);
+wpi.digitalWrite(pin, new_val);
 
 // read the value
-wpi.pinMode(16, wpi.modes.INPUT);
-console.log(wpi.digitalRead(16));
+wpi.pinMode(pin, wpi.modes.INPUT);
+console.log(wpi.digitalRead(pin));
