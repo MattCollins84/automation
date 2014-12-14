b0d70b30c5370d65403a535021ab1775cf56fe79
Matt Collins <matt@strikesandgutters.com>
2014-12-14 14:16:15 +0000

    script

--- alt.js
+++ alt.js
@@ -6,5 +6,8 @@ var pin = argv.pin;
 gpio.open(pin, "input", function(err) {     // Open pin 16 for input
   gpio.read(pin, function(err, value) {
     console.log(err, value)
+    gpio.close(pin, function(err) {
+      console.log('done');
+    });
   });
 });
