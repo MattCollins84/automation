a7137c9d0a4af10130595e19714c4c5704ffcc8c
Matt Collins <matt@strikesandgutters.com>
2014-12-14 14:13:54 +0000

    script

---
+++ alt.js
@@ -0,0 +1,10 @@
+var gpio = require("pi-gpio");
+var argv = require('optimist').argv
+
+var pin = argv.pin;
+
+gpio.open(pin, "input", function(err) {     // Open pin 16 for input
+  gpio.read(pin, function(err, value) {
+    console.log(err, value)
+  });
+});
